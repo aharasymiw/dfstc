0f5c1bfb829410e92aeb8eab1265a60032de9da0
Andrew Harasymiw <aharasymiw@gmail.com>
2016-02-01 12:36:47 -0600

    cleaned up formating, removed some auth0 dependancies

--- server/public/scripts/controllers/form.js
+++ server/public/scripts/controllers/form.js
@@ -1,17 +1,17 @@
 app.controller('FormCtrl', ['$scope', '$http',
 'store', function($scope, $http, store) {
 
-		$scope.submit = function(){
-			$http({
-				method: 'POST',
-				url: '/api/clients',
-				data: $scope.form
-			}).then(function successCallback(response) {
+  $scope.submit = function() {
+    $http({
+      method: 'POST',
+      url: '/api/clients',
+      data: $scope.form
+    }).then(function successCallback(response) {
 
-			}, function errorCallback(response) {
-				console.log(response);
-			});
+    }, function errorCallback(response) {
+      console.log(response);
+    });
 
-		}
+  };
 
 }]);
