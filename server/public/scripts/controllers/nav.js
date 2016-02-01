e2190f1647854f3175da5c2669c33d4fe52d5169
Andrew Harasymiw <aharasymiw@gmail.com>
2016-02-01 12:01:20 -0600

    fixed formating to comply with .jscsrc

--- server/public/scripts/controllers/nav.js
+++ server/public/scripts/controllers/nav.js
@@ -1,5 +1,6 @@
 app.controller('NavCtrl', ['$scope', '$http', 'store',
-'$location','$ui.bootstrap', 'ngAside', function($scope, $http, store, $location, $aside) {
+'$location','$ui.bootstrap', 'ngAside', function($scope, $http, store,
+$location, $aside) {
   $scope.logout = function() {
     alert('loged out');
     auth.signout();
@@ -9,28 +10,26 @@ app.controller('NavCtrl', ['$scope', '$http', 'store',
   };
 }]);
 
-function htmlbodyHeightUpdate(){
-  var height3 = $( window ).height()
-  var height1 = $('.nav').height()+50
-  height2 = $('.main').height()
-  if(height2 > height3){
-    $('html').height(Math.max(height1,height3,height2)+10);
-    $('body').height(Math.max(height1,height3,height2)+10);
-  }
-  else
-  {
+function htmlbodyHeightUpdate() {
+  var height3 = $(window).height();
+  var height1 = $('.nav').height() + 50;
+  height2 = $('.main').height();
+  if(height2 > height3) {
+    $('html').height(Math.max(height1,height3,height2) + 10);
+    $('body').height(Math.max(height1,height3,height2) + 10);
+  } else {
     $('html').height(Math.max(height1,height3,height2));
     $('body').height(Math.max(height1,height3,height2));
   }
 
 }
-$(document).ready(function () {
-  htmlbodyHeightUpdate()
-  $( window ).resize(function() {
-    htmlbodyHeightUpdate()
+$(document).ready(function() {
+  htmlbodyHeightUpdate();
+  $(window).resize(function() {
+    htmlbodyHeightUpdate();
   });
-  $( window ).scroll(function() {
-    height2 = $('.main').height()
-    htmlbodyHeightUpdate()
+  $(window).scroll(function() {
+    height2 = $('.main').height();
+    htmlbodyHeightUpdate();
   });
 });
