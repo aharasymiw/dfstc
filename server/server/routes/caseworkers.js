10b03e9f5fe64fbde76a8fe3cf2bcd24fc733d52
cwklausing <cwklausing@gmail.com>
2016-01-31 23:37:38 -0600

    Update caseworker form, route, model

--- server/server/routes/caseworkers.js
+++ server/server/routes/caseworkers.js
@@ -3,14 +3,19 @@ var router = express.Router();
 var path = require('path');
 
 var mongoose = require('mongoose');
-var Caseworkers = require('../models/caseworkers');
+var Caseworker = require('../models/caseworkers');
 
 router.get('/', function(req, res, next) {
   res.send('');
 });
 
 router.post('/', function(req, res, next) {
-  res.send('');
+  var caseworkerForm = new Caseworker({
+    cwFirstName: req.body[0],
+    cwLastName: String[1],
+    cwEmail: String[2],
+    cwOrg: String[3],
+  });
 });
 
 router.put('/', function(req, res, next) {
