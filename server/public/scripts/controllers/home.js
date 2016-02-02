cef737f81346df56e93e5ea22e8871606fa6be25
adia_a <Adia@Princess.local>
2016-02-02 11:17:28 -0600

    added route to home controller

--- server/public/scripts/controllers/home.js
+++ server/public/scripts/controllers/home.js
@@ -1,4 +1,14 @@
 app.controller('HomeCtrl', ['$scope', '$http',
 'store', function($scope, $http, store) {
-
+  //$scope.submit = function() {
+  //  $http({
+  //    method: 'POST',
+  //    url: '/login',
+  //    data: $scope.form
+  //  }).then(function successCallback(response) {
+  //
+  //  }, function errorCallback(response) {
+  //    console.log(response);
+  //  });
+  //};
 }]);
