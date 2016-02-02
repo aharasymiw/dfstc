7b4464fcb6f0d8f8766594f0702722f26eff2d2a
cwklausing <cwklausing@gmail.com>
2016-02-02 10:00:57 -0600

    added email feature to admin_caseworker controller

--- server/public/scripts/controllers/admin_caseworkers.js
+++ server/public/scripts/controllers/admin_caseworkers.js
@@ -6,10 +6,20 @@ app.controller('AdminCaseworkersCtrl', ['$scope', '$http',
       url: '/api/caseworkers',
       data: $scope.form
     }).then(function successCallback(response) {
-
+      createAccount($scope.form.cwEmail);
     }, function errorCallback(response) {
       console.log(response);
     });
 
+    var createAccount = function(email) {
+      $http({
+        method: 'POST',
+        url: '/api/jauth/caseworker',
+        data: email
+      }).then(function successCallback(response) {
+
+      }, function errorCallback(response) {
+        console.log(response);
+      });
   };
 }]);
