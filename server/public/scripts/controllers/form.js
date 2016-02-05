a3fbd71abb833c4e02006110c6611e6c81a32986
cwklausing <cwklausing@gmail.com>
2016-02-05 14:51:41 -0600

    Merged Nick's styling with develop branch

--- server/public/scripts/controllers/form.js
+++ server/public/scripts/controllers/form.js
@@ -1,6 +1,7 @@
 app.controller('FormCtrl', ['$scope', '$http', 'store', function($scope, $http, store) {
 
   $scope.submit = function() {
+    console.log('FIRE!!!');
     $http({
       method: 'POST',
       url: '/api/clients',
@@ -13,18 +14,17 @@ app.controller('FormCtrl', ['$scope', '$http', 'store', function($scope, $http, 
 
   };
 
-}]);
-
-    app.controller('FormCtrl', function($scope) {
-      $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-      'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-      'WY').split(' ').map(function(state) {
-        return {abbrev: state};
-      })
-    })
+  $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+  'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+  'WY').split(' ').map(function(state) {
+    return {abbrev: state};
+  });
+}])
     .config(function($mdThemingProvider) {
-      // Configure a dark theme with primary foreground yellow
-      $mdThemingProvider.theme('docs-dark', 'default')
-          .primaryPalette('yellow')
-          .dark();
-    });
+  // Configure a dark theme with primary foreground yellow
+  $mdThemingProvider.theme('docs-dark', 'default')
+      .primaryPalette('yellow')
+      .dark();
+});
+
+
