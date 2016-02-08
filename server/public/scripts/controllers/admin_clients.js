8245f75c067550cb02d533fb3e7c288be146f996
cwklausing <cwklausing@gmail.com>
2016-02-08 17:23:47 -0600

    Adding edit buttons for admin clients table

--- server/public/scripts/controllers/admin_clients.js
+++ server/public/scripts/controllers/admin_clients.js
@@ -4,12 +4,16 @@ app.controller('AdminClientsCtrl', ['$scope', '$http',
   //Returns an array of all records in the clients collection
   $scope.rowCollection = [];
   $scope.data = [].concat($scope.rowCollection);
-
+  $scope.seeFullClient = function(row) {
+    var index = $scope.rowCollection.indexOf(row);
+    if (index !== -1) {
+      console.log(row);
+    }
+  };
   $http.get('api/clients/all').then(function(res) {
     console.log(res);
     $scope.rowCollection = res.data;
     $scope.data = [].concat($scope.rowCollection);
     console.log($scope.rowCollection);
   }, function(err) {console.log(err.message);});
-
 }]);
