fc756cacffe78894e021139a064a0a1b66987e70
adia_a <adia.alderson@gmail.com>
2016-02-25 14:04:17 -0600

    middleware function added, still need to add functionality

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ var routes = require('./server/routes/index');
 var login = require('./server/routes/login');
 var jauth = require('./server/routes/jauth');
 var catchall = require('./server/routes/catchall');
+var authCheck = require('./server/services/authMiddleware');
 
 var app = express();
 
@@ -24,14 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/api/*', jwt({secret: 'supersecret'}));
 
-app.use('/api/appointments', appointments);
-app.use('/api/caseworkers', caseworkers);
-app.use('/api/clients', clients);
+app.use('/api/appointments', authCheck(), appointments);
+app.use('/api/caseworkers', authCheck(), caseworkers);
+app.use('/api/clients', authCheck(), clients);
 app.use('/login', login);
 app.use('/api/jauth', jauth);
 
 app.use('/', routes);
-app.use('/*', catchall);
+app.use('/*', authCheck(), catchall);
 
 /** ---------- MONGOOSE CONNECTION HANDLING ---------- **/
 var dbURI = 'mongodb://localhost:27017/dfstc';
---
+++ server/server/services/authMiddleware.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Adia on 2/25/16.
+ */
+
+module.exports = function() {
+
+  return function(req, res, next) {
+    console.log('I work!!!!');
+  };
+
+};
