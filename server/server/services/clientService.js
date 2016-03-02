eea6b031a3bc4a241ef553491a37b234911cb99c
cwklausing <cwklausing@gmail.com>
2016-03-02 11:30:29 -0600

    Commenting out client service

--- server/server/services/clientService.js
+++ server/server/services/clientService.js
@@ -21,6 +21,7 @@ var clientService = {
   },
 
   editClient: function(data) {
+    //Converts data._id into an id type, which can query mongo database.
     var ObjectId = mongoose.Types.ObjectId;
     var id = ObjectId(data._id);
     //Object containing all the fields in the form, set equal to the data value.
