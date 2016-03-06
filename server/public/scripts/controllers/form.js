4afe1275a8a566afecd97a98e6bae3689d3a1127
Andrew Harasymiw <aharasymiw@gmail.com>
2016-03-06 13:14:42 -0600

    Cleaned up form.js to follow style guide and removed extra curly braces.

--- server/public/scripts/controllers/form.js
+++ server/public/scripts/controllers/form.js
@@ -1,21 +1,21 @@
-app.controller('FormCtrl', ['$scope', '$http', 'store', function($scope, $http, store) {
+app.controller('FormCtrl', ['$scope', '$http', 'store',
+function($scope, $http, store) {
 
-  {
-    $scope.myDate = new Date();
-    $scope.minDate = new Date(
-        $scope.myDate.getFullYear(),
-        $scope.myDate.getMonth() - 2,
-        $scope.myDate.getDate());
-    $scope.maxDate = new Date(
-        $scope.myDate.getFullYear(),
-        $scope.myDate.getMonth() + 2,
-        $scope.myDate.getDate());
-    $scope.onlyWeekendsPredicate = function(date) {
-      var day = date.getDay();
-      return day === 0 || day === 6;
-    };
+  $scope.myDate = new Date();
+  $scope.minDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() - 2,
+      $scope.myDate.getDate());
+  $scope.maxDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() + 2,
+      $scope.myDate.getDate());
+  $scope.onlyWeekendsPredicate = function(date) {
+    var day = date.getDay();
+    return day === 0 || day === 6;
+  };
 
-    var retrieveAppointments = function() {
+  var retrieveAppointments = function() {
     $http({
       method: 'GET',
       url: '/api/appointments'
@@ -26,11 +26,12 @@ app.controller('FormCtrl', ['$scope', '$http', 'store', function($scope, $http, 
     });
   };
 
-    var updateAppointments = function() {
+  var updateAppointments = function() {
     $http({
       method: 'POST',
       url: '/api/appointments/update',
-      data: {_id: $scope.form.appointment, email: $scope.form.clientEmail, appointmentType: $scope.form.appointmentType}
+      data: {_id: $scope.form.appointment, email: $scope.form.clientEmail,
+      appointmentType: $scope.form.appointmentType}
     }).then(function successCallback(response) {
       saveClient();
       console.log(response);
@@ -39,30 +40,27 @@ app.controller('FormCtrl', ['$scope', '$http', 'store', function($scope, $http, 
     });
   };
 
-    var saveClient = function() {
+  var saveClient = function() {
     console.log('FIRE!!!');
     $http({
       method: 'POST',
       url: '/api/clients',
       data: $scope.form
     }).then(function successCallback(response) {
-
     }, function errorCallback(response) {
       console.log(response);
     });
-
   };
 
-    retrieveAppointments();
+  retrieveAppointments();
 
-    $scope.submit = function() {
+  $scope.submit = function() {
     updateAppointments();
   };
-  };
 
-  $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-  'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-  'WY').split(' ').map(function(state) {
+  $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME ' +
+   'MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX ' +
+   'UT VT VA WA WV WI WY').split(' ').map(function(state) {
     return {abbrev: state};
   });
 }])
